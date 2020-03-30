577d6d5a4feae9a60672054cb8ecb80d959e3e39
Roman Hudyma <roman.hudymaa@gmail.com>
2020-03-30 21:34:25 +0300

    Updates

---
+++ precache-manifest.194c6265442ee96c3f88250245c01990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be36a46c2647b649d0c33664710b27de",
+    "url": "/rw-hw/index.html"
+  },
+  {
+    "revision": "876fae4ac6db316ea0ee",
+    "url": "/rw-hw/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "644a094db5d654dfe8d9",
+    "url": "/rw-hw/static/css/main.0d65826b.chunk.css"
+  },
+  {
+    "revision": "876fae4ac6db316ea0ee",
+    "url": "/rw-hw/static/js/2.61bf182d.chunk.js"
+  },
+  {
+    "revision": "644a094db5d654dfe8d9",
+    "url": "/rw-hw/static/js/main.27e22f59.chunk.js"
+  },
+  {
+    "revision": "3f786434fe8290576cef",
+    "url": "/rw-hw/static/js/runtime~main.5a31a2a8.js"
+  }
+]);
